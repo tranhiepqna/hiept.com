4ae414d5933dcf565665b20afdc9f3bc673133ab
tranhiepqna <tranhiepqna@gmail.com>
2022-06-28 23:22:29 +0700

    feat: create hello world post

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import profilePic from '../public/me.png';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Image
+        src={profilePic}
+        alt="Picture of the author"
+        width={75}
+        height={75}
+        className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4"
+      />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
